15e59012d7e1c1dd632044f59aa48cabc5be2765
mywyau <47248104+mywyau@users.noreply.github.com>
2025-04-07 14:23:38 +0100

    add roman numerals

--- app/src/main/java/easy/RomanNumerals.java
+++ app/src/main/java/easy/RomanNumerals.java
@@ -3,7 +3,8 @@ package easy;
 public class RomanNumerals {
 
     public String intToRoman(int num) {
-        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};  // arrays faster than Map traversal
         String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
         StringBuilder result = new StringBuilder();
@@ -16,10 +17,5 @@ public class RomanNumerals {
         }
 
         return result.toString();
-    }
-
-       public static void main(String[] args) {
-
-        // System.out.println(intToRoman(3999));
     }
 }
