1d7d17905f70a821e57127b97ea3bbbccb30bbc3
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-07-25 16:36:29 +0100

    adding notes to merge two sorted lists

--- app/src/main/java/leetcode/easy/mergeTwoSortedLists/MergeTwoSortedLists.java
+++ app/src/main/java/leetcode/easy/mergeTwoSortedLists/MergeTwoSortedLists.java
@@ -8,32 +8,51 @@ package leetcode.easy.mergeTwoSortedLists;
 
 public class MergeTwoSortedLists {
 
+    // public means accessible | return type ListNode | method name | parameters
+
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode dummy = new ListNode(0); // dummy head
-        ListNode current = dummy;
 
-        while (list1 != null && list2 != null) {
-            if (list1.val < list2.val) {
-                current.next = list1;
-                list1 = list1.next;
+        ListNode dummy = new ListNode(0); // dummy head, initiate head, we accumulate here and use this to create and
+                                          // save the output.
+
+        ListNode current = dummy; // current points to the last node in the result list
+
+        while (list1 != null && list2 != null) { // we iterate through out lists of numbers, and for each node do the
+                                                 // following:
+
+            if (list1.val < list2.val) { // if node 1 < node 2
+                current.next = list1; // then we update our accumulator with our node
+                list1 = list1.next; // update list1 to be the next node
             } else {
-                current.next = list2;
-                list2 = list2.next;
+                current.next = list2; // else we use list2, then we update our accumulator with the node from list 2
+                list2 = list2.next; // update list2 to be the next node since we have used the current node
             }
-            current = current.next;
+
+            current = current.next; // move the current pointer forward in the merged list
         }
 
+        // Once we breakout of the loop above
         // Attach the remaining nodes
+        // since we run this after the while loop, it’s not possible for both to be non-null at this point.
+        
+        // This loop continues as long as both lists are not null.
+
+        //  So, it only stops when at least one is null. That means:
+        //  Either list1 == null, or
+        //  list2 == null, or
+        //  Both are null
         if (list1 != null) {
-            current.next = list1;
+            current.next = list1; // set any remaining nodes in list1
         } else {
-            current.next = list2;
+            current.next = list2; // If list1 is null and list2 still has nodes, we just attach the rest of list2
+                                  // directly, because it's already sorted.
         }
 
-        return dummy.next;
+        return dummy.next; // “We return dummy.next at the end to skip the dummy node itself.”
     }
 
     public static void main(String[] args) {
+
         ListNode l1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode l2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
