e173813220531e620c362e45365d54823b55d1c4
michael-hmrc <47248104+michael-hmrc@users.noreply.github.com>
2025-09-08 18:16:21 +0100

    clean up

--- app/src/main/java/dsa/linked_list/ReversedLinkedList.java
+++ app/src/main/java/dsa/linked_list/ReversedLinkedList.java
@@ -1,8 +1,5 @@
 package dsa.linked_list;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ReversedLinkedList {
 
     public ListNode reversedLinkedList(ListNode head) {
@@ -12,28 +9,13 @@ public class ReversedLinkedList {
 
         while (curr != null) {
             ListNode next = curr.next; // store next
-            curr.next = prev; // store next
+            curr.next = prev; // current.next set to previous
             prev = curr; // ✅ move prev forward
             curr = next;
         }
 
         return prev;
     }
-
-    // Cycle Detection is one of the most common linked list interview questions. It tests how well you handle pointers. Let’s break it down:
-
-    public boolean hasCycleHashSet(ListNode head) {
-        Set<ListNode> seen = new HashSet<>();
-        while (head != null) {
-            if (seen.contains(head))
-                return true;
-            seen.add(head);
-            head = head.next;
-        }
-        return false;
-    }
-
-    // Given a linked list, determine if it has a cycle.
 
     public static void main(String[] args) {
 
